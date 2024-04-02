50b620deb132fdd860e76e13ab4f666604cf3524
v6wind <35835096+v6wind@users.noreply.github.com>
2024-04-03 07:41:16 +0800

    Update ifeng.php

--- api/ifeng.php
+++ api/ifeng.php
@@ -7,31 +7,19 @@
       'fhzw' => 'f7f48462-9b13-485b-8101-7b54716411ec',  //中文台
       'fhhk' => '15e02d92-1698-416c-af2f-3e9a872b4d78',  //香港台
       ];
-    $url = "https://m.fengshows.com/api/v3/hub/live/auth-url?live_id={$n[$id]}&live_qa=HD";
-    $flv = get_headers(json_decode(file_get_contents($url))->data->live_url,1)['Location'];
-    $arr = [
-           '119.176.27.164',
-           '153.35.100.175',
-           '140.249.28.99',
-           '183.201.202.143',
-           '60.188.68.36',
-           '150.139.157.212',
-           '182.242.47.244',
-           ];
-    $ip = $arr[array_rand($arr)];
-    $flv = preg_replace("/qctv.fengshows.cn/","{$ip}/qctv.fengshows.cn",$flv);
-    $m3u8 = preg_replace('/.flv/','.m3u8',$flv);
-
-    if($fmt == 'flv'){
-       header('Location:'.$flv);
-       //echo $flv;
+    if($fmt == 'hls') $url = "https://m.fengshows.com/api/v3/hub/live/auth-url?stream_type=hls&live_id={$n[$id]}&live_qa=HD";
+    if($fmt == 'flv') $url = "https://m.fengshows.com/api/v3/hub/live/auth-url?stream_type=flv&live_id={$n[$id]}&live_qa=HD";
+    $live = get_headers(json_decode(file_get_contents($url))->data->live_url,1)['Location'];
+    if($fmt == 'flv') {
+       header('Location:'.$live);
+       //echo $live;
        }
-    if($fmt == 'hls'){
+    if($fmt == 'hls') {
        $burl = "http://qctv.fengshows.cn/live/";
-       $cur = preg_replace("/(.*?.ts)/i", $burl."$1",file_get_contents($m3u8));
-       $cur = preg_replace("/48-/","72-",$cur);
+       $cur = preg_replace("/(.*?.ts)/i", $burl."$1",file_get_contents($live));
+       if($id == 'fhzw') $cur = $cur;
+       else $cur = preg_replace("/48-/","72-",$cur);
        header("Content-Type: application/vnd.apple.mpegurl");
-       header("Content-Disposition: inline; filename=index.m3u8");
-       echo $cur;
-    }
+       print_r($cur);
+       }
     ?>
