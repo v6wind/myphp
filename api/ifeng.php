0993ca4308fbf8e222567cb9794e8c5a7bff123c
v6wind <35835096+v6wind@users.noreply.github.com>
2024-04-03 09:51:02 +0800

    Update ifeng.php

--- api/ifeng.php
+++ api/ifeng.php
@@ -1,17 +1,46 @@
 <?php
+header("Content-Type: text/json; charset=UTF-8");
 $id = isset($_GET['id'])?$_GET['id']:'fhzx';
-$tv = [
-  'fhzx' => '7c96b084-60e1-40a9-89c5-682b994fb680',  //資 訊 台
-  'fhzw' => 'f7f48462-9b13-485b-8101-7b54716411ec',  //中 文 台
-  'fhhk' => '15e02d92-1698-416c-af2f-3e9a872b4d78',  //香 港 台
-  ];
-$url = "https://m.fengshows.com/api/v3/hub/live/auth-url?live_id={$tv[$id]}&live_qa=FHD";
-$h=[
-  'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X)',  
-  'token:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiIzMWUzZmVjMC1lY2IzLTExZWQtOWUxNS1mM2FiZjliZjhkOTYiLCJuYW1lIjoiIiwidmlwIjowLCJqdGkiOiJqQm5nMXBvZlQiLCJpYXQiOjE2ODM0NDg5ODksImV4cCI6MTY4NjA0MDk4OX0.0r8PuLetMiusCJul2tuPRzU8fnhxhqxBoycDV0_vKxI',
-  ];
-$cont = stream_context_create(['http'=>['header'=>$h]]);
-$playurl = json_decode(file_get_contents($url, null, $cont))->data->live_url;
-header('Location:'.$playurl);
-//echo $playurl;
-?>
+$tv = array(
+  'fhzx' => '4',  //資 訊 台
+  'fhzw' => '5',  //中 文 台
+  'fhhk' => '6',  //香 港 台
+  );
+$url = 'http://m.fengshows.com/api/v3/live?live_type=tv';
+$response = get_data($url);
+$channels = json_decode($response);
+foreach ($channels as $channel) {
+  if($channel->order==$tv[$id]){
+    $channelId = $channel->_id;
+    break; 
+  }    
+}
+$info = get_url($channelId,'FHD');
+if($info->status !== '0'){
+  $info = get_url($channelId,'HD');
+}
+$liveUrl = $info->data->live_url;
+header('Location:'.$liveUrl);
+
+function get_url($cid, $qa){
+  $url = "https://m.fengshows.com/api/v3/hub/live/auth-url?live_id={$cid}&live_qa={$qa}";
+  $response = get_data($url);
+  $data = json_decode($response);
+  return $data;
+}
+function get_data($url){
+$header=array(
+  'fengshows-client: app(ios,5040718)',
+  'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/98.0.4758.85 Mobile/15E148 Safari/604.1',
+  'token:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiIzMWUzZmVjMC1lY2IzLTExZWQtOWUxNS1mM2FiZjliZjhkOTYiLCJuYW1lIjoiIiwidmlwIjowLCJqdGkiOiJqQm5nMXBvZlQiLCJpYXQiOjE2ODM0NDg5ODksImV4cCI6MTY4NjA0MDk4OX0.0r8PuLetMiusCJul2tuPRzU8fnhxhqxBoycDV0_vKxI', 
+);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+$data = curl_exec($ch);
+curl_close($ch);
+return $data;
+}
