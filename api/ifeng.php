9443de2b5c519336fbc0d81b5a5eb4b73021492f
v6wind <35835096+v6wind@users.noreply.github.com>
2024-04-03 09:38:28 +0800

    Update ifeng.php

--- api/ifeng.php
+++ api/ifeng.php
@@ -1,89 +1,27 @@
 <?php
-   
-    $id = $_GET['id']-1; // 1 = 资讯  2 = 中文  3 = 深圳旁边
+date_default_timezone_set("Asia/Shanghai");
+$id = $_GET['id'] ?? 'zx';
+$n = [
+    'ws'=>'0701pcc72',//凤凰卫视
+    'zx'=>'0701pin72',//凤凰资讯
+    'hk'=>'0701phk72',//凤凰香港
+];
 
-    $public_key = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCsMwDFK9QtawzMO7Df8clvO/gmVbpJ10CRz8uvq2pH4+700yURMgHWiMsIsUza0MPEgIffCAFtn4n0wk+arEcm2/vQd+5ebe0urgpGjl64SST0BxMSNgOWHCmKe7jVNWx6OusSUasDD+ieXChjjMDAaoH0Qx2g2Lg5Zu00abJv+QIDAQAB';
-    $public_key = "-----BEGIN PUBLIC KEY-----\n".wordwrap($public_key, 64, "\n", true) . "\n-----END PUBLIC KEY-----";
-    $public_key_a = openssl_pkey_get_public($public_key);
-   
-    $ticket = '';
-   
-    if(openssl_public_encrypt('$2k4Yk84Mo0O!',$encrypt_data,$public_key_a))
-    {
-        $ticket = base64_encode($encrypt_data);
-    }
+if (strstr($id, '4')) {
+    $seq = intval(time() / 3.029 + 1134263867);
+} elseif (strstr($id, '5')) {
+    $seq = intval(time() / 3.026 + 1130361490);
+} elseif (strstr($id, '6')) {
+    $seq = intval(time() / 3.008 + 1130361113);
+} else {
+    $seq = intval(time() / 4.000 + 1269967460)-3;       //-3以实际测试调整
+}
 
-    $headers = array(
-        'fengshows-client: app(android,5010208);23',
-        'User-Agent: okhttp/3.11.0'
-    );
-    $bstrURL = "https://m.fengshows.com/api/v3/live?live_type=tv&page=1&page_size=15";
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $bstrURL);                  
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    $data = curl_exec($ch);
-    curl_close($ch);
-    $json = json_decode($data);
-
-    $vid = $json[$id]->_id;
-
-    // 不知道是否为动态ID
-    // $vid = '7c96b084-60e1-40a9-89c5-682b994fb680';
-    //7c96b084-60e1-40a9-89c5-682b994fb680  资讯台
-    //f7f48462-9b13-485b-8101-7b54716411ec  中文台
-    //15e02d92-1698-416c-af2f-3e9a872b4d78  深圳旁边台
-
-    /**
-     *  获取token
-     *  token 有一定时效性，建议cache而非频繁登陆，容易ban id
-     */
-
-    $loginData = [
-        "phone"=>"凤凰秀账号", // 手机号码登陆方式，这里是你的手机号
-        "password"=>"凤凰密码", // 密码
-        "ticket"=>$ticket,
-        "code"=>"86"
-
-    ];
-   
-    $headers[] = 'Content-Type: application/json; charset=utf-8'; // 增加 content-type
-    $bstrURL = "https://m.fengshows.com/user/oauth/login";
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $bstrURL);                  
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_POST,TRUE);
-    curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($loginData));
-    $data = curl_exec($ch);
-   
-    curl_close($ch);
-    $json = json_decode($data);
-    $token = $json->data->token;  
-    array_pop($headers); // 移除 content-type 头部
-    $headers[] = 'token: '.$token;  // 加入 token 头
-
-    // token 更新接口
-    // https://m.fengshows.com/user/oauth/update
-    // 使用cache的时候
-
-    // 获取播放链接
-    $bstrURL = 'https://m.fengshows.com/api/v3/hub/live/auth-url?live_id='.$vid.'&live_qa=fhd'; // fhd需要提交登陆token
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $bstrURL);                  
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    $data = curl_exec($ch);
-    curl_close($ch);
-    $json = json_decode($data);
-    $liveURL = $json->data->live_url;
-   
-    header("location:". $liveURL);
-
-?>
+$content = "#EXTM3U\n#EXT-X-VERSION:3\n#EXT-X-TARGETDURATION:4\n#EXT-X-MEDIA-SEQUENCE:$seq\n";
+for($i=0;$i<3;$i++){
+    $content .= "#EXTINF:4.000,\n";
+    $content .= "http://qctv.fengshows.cn/live/".$n[$id]."-".strval($seq+$i).".ts\n";
+}
+header("Content-Type: application/vnd.apple.mpegurl");
+header("Content-Disposition: attachment; filename=playlist.m3u8");
+echo $content;
