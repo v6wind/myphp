cbdb354cf840af126edf9581fba2752ad6d70b2a
v6wind <35835096+v6wind@users.noreply.github.com>
2024-04-03 08:14:29 +0800

    Update ifeng.php

--- api/ifeng.php
+++ api/ifeng.php
@@ -1,35 +1,43 @@
 <?php
-$id = isset( $_GET[ 'id' ] ) ? $_GET[ 'id' ] : '3'; // 1=凤凰资讯 2=凤凰中文 3=凤凰HK
-$id = $id - 1;
-$idUrl = 'https://m.fengshows.com/api/v3/live?live_type=tv&page=1&page_size=15';
+$id = $_GET['id'] ?? '1';
+$channelId = [
+    '1' => '7c96b084-60e1-40a9-89c5-682b994fb680',  //资讯台
+'2' => 'f7f48462-9b13-485b-8101-7b54716411ec',  //中文台
+'3' => '15e02d92-1698-416c-af2f-3e9a872b4d78',  //香港台
+];
+$type = $_GET['qa'] ?? 'HD';
+$authUrl = 'https://m.fengshows.com/api/v3/hub/live/auth-url?stream_type=hls&live_id=';
+
 $headers = array(
-  'fengshows-client: app(ios,5020201);iPhone14,5;15.6',
-  'Cookie: acw_tc=0bc159c416601310655302310e60a16525c75b28a289968d7981e08cf77999',
-  'User-Agent: FengWatch/5.2.2 (iPhone; iOS 15.6; Scale/3.00)'
+    'Fengshows-Client: app(ios,5040873);iPhone14,5;16.6',
+    'Cookie: acw_tc=0bc159c416601310655302310e60a16525c75b28a289968d7981e08cf77999',
+    'User-Agent: FengWatch/5.4.8 (com.phoenixtv.videoapp; build:5040873; iOS 16.6.0) Alamofire/5.6.4'
 );
-$data = curlClient( $idUrl, $headers );
-$json = json_decode( $data );
-$liveId = $json[ $id ]->_id;
 
-$authUrl = 'https://m.fengshows.com/api/v3/hub/live/auth-url?live_id=' . $liveId . '&live_qa=HD';
-$data = curlClient( $authUrl, null );
-$playUrl = json_decode( $data, true )[ 'data' ][ 'live_url' ];
-#echo $playUrl;
-//header( 'location:' . str_replace( '.flv', '.m3u8', $playUrl ) );
-header( 'location:'. $playUrl );
-
-function curlClient( $url, $headers ) {
-  $ch = curl_init();
-  curl_setopt( $ch, CURLOPT_URL, $url );
-  curl_setopt( $ch, CURLOPT_RETURNTRANSFER, TRUE );
-  if ( !empty( $headers ) ) {
-    curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
-  }
-  curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, FALSE );
-  curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, FALSE );
-  $data = curl_exec( $ch );
-  curl_close( $ch );
-  return $data;
+$liveUrl = json_decode(get_data($authUrl . $channelId[$id] . '&live_qa=' . $type, $headers), true)['data']['live_url'];
+$m3u8 = get_data($liveUrl, $headers);
+$serverUrl = 'http://qctv.fengshows.cn/live/';
+if ($type == 'audio') {
+    $playUrl = preg_replace('/(\w{7})(_audio-\d+\.ts)\?.*/', $serverUrl . '$1$2', $m3u8);
+} else {
+    $playUrl = preg_replace('/(\w{7})(\d{2})(-\d+\.ts)\?.*/', $serverUrl . '${1}72$3', $m3u8);
 }
+header('Content-Type: application/vnd.apple.mpegURL');
+header('Content-Disposition: attachment; filename=playlist.m3u8');
+echo $playUrl;
 
-?>
+function get_data($url, $headers){
+    $curl = curl_init();
+    if (str_starts_with($url, 'https')) {
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    }
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+    $data = curl_exec($curl);
+    curl_close($curl);
+    return $data;
+}
