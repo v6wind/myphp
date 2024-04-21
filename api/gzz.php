6c11e2d5c991b6338013a193ee120f18fd76f87e
v6wind <35835096+v6wind@users.noreply.github.com>
2024-04-21 18:49:29 +0800

    Update gzz.php

--- api/gzz.php
+++ api/gzz.php
@@ -1,5 +1,5 @@
 <?php
-$data = json_decode(file_get_contents("https://gzbn.gztv.com:7443/plus-cloud-manage-app/liveChannel/queryLiveChannelList?type=1"))->data;//id=31-36
+$data = json_decode(file_get_contents("http://gzbn.gztv.com:7443/plus-cloud-manage-app/liveChannel/queryLiveChannelList?type=1"))->data;//id=31-36
 $count = count($data);
 for($i=0;$i<$count;$i++){
 if($data[$i]->stationNumber == $_GET["id"]){
