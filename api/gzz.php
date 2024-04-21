7b4c3af47b265796c339c54442d3b983bfe96b51
v6wind <35835096+v6wind@users.noreply.github.com>
2024-04-21 18:23:28 +0800

    Update gzz.php

--- api/gzz.php
+++ api/gzz.php
@@ -1,19 +1,29 @@
 <?php
-$id=$_GET['id'];
-$n = [
-    "zhonghe" => 31,//广州综合
-    "xinwen" => 32,//广州新闻
-    "jingsai" => 35,//广州竞赛
-    "yingshi" => 36,//广州影视
-    "fazhi" => 34,//广州法治
-    "shenghuo" => 33,//广州南国都市
+    $id = $_GET['id'] ?? 'zhonghe';
+    $programs = [
+        'zhonghe' => 0,     //广州综合
+        'xinwen' => 1,      //广州新闻
+        'fazhi' => 2,     //广州法治
+        'jingsai' => 3,     //广州竞赛
+        'yingshi' => 4,       //广州影视
+        'shenghuo' => 5,    //广州南国都市
     ];
-$data = json_decode(file_get_contents("https://gzbn.gztv.com:7443/plus-cloud-manage-app/liveChannel/queryLiveChannelList?type=1"))->data;//id=31-36
-$count = count($data);
-for($i=0;$i<$count;$i++){
-if($data[$i]->stationNumber == $n[$id]){
-$playurl = $data[$i]->httpUrl;
-break;
-}}
-header("Location: {$playurl}",true,302);
+    $url = 'https://gzbn.gztv.com:7443/plus-cloud-manage-app/liveChannel/queryLiveChannelList?type=1';
+    $headers = [
+        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/99.0.4844.84 Safari/537.36 HBPC/12.1.3.3',
+        'Host: gzbn.gztv.com',
+        'Origin: gzbn.gztv.com',
+        'Referer: gzbn.gztv.com',
+    ];
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    $result = json_decode($data)->data;
+    $index = $programs[$id];
+    $playUrl = $result[$index]->httpUrl ?? '';
+    header('location:'.$playUrl);
 ?>
