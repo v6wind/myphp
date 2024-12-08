c50b5e7ae7c917c2ed9da5ca285cb2a0462572f8
v6wind <35835096+v6wind@users.noreply.github.com>
2024-12-09 02:18:50 +0800

    Update ifeng.php

--- api/ifeng.php
+++ api/ifeng.php
@@ -32,7 +32,7 @@ function get_data($url){
 $header=array(
   'fengshows-client: app(ios,5040718)',
   'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/98.0.4758.85 Mobile/15E148 Safari/604.1',
-  'token:eyJfaWQiOiI0OTQ3M2U4MC00Yjc3LTExZWMtYjM4My0xZDkxOWNhOTViOWQiLCJuYW1lIjoiVjZXaW5kIiwidmlwIjowLCJqdGkiOiJUNUp6M3dBcHYiLCJyZWdpc3RyYXRpb25faWQiOiIxNzE5NzZmYThhNDFmMjFlNDI4IiwiaWF0IjoxNzMzNjgxNTQ2LCJleHAiOjE3MzYyNzM1NDZ9.9TfWmwkaDpx0oGjjZPppYA57LtfVGkz82DCKxDs1XyY', 
+  'token:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI0OTQ3M2U4MC00Yjc3LTExZWMtYjM4My0xZDkxOWNhOTViOWQiLCJuYW1lIjoiVjZXaW5kIiwidmlwIjowLCJqdGkiOiI5ZnFMc0RDOVkiLCJyZWdpc3RyYXRpb25faWQiOiIxNzE5NzZmYThhNDFmMjFlNDI4IiwiaWF0IjoxNzMzNjgxODQxLCJleHAiOjE3MzYyNzM4NDF9.o2iSGTyEoI8r5cmSsmr5ix2p8XaL384kpX1invEHLr8', 
 );
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
